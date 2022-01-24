0a0764ebb673071e562e2f06b188de3ea4f484c3
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2022-01-24 13:16:53 -0300

    reshape2

--- ui.R
+++ ui.R
@@ -17,7 +17,7 @@ library(kableExtra)
 library(highcharter)
 library(ggplot2)
 library(tidyverse)
-
+library(reshape2)
 ##########################
 ### obtencao dos dados ###
 ##########################
