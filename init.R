7458e5f820d709f1888bae85c10862305a4e2b18
Alessandro Pereira Torres De Sá Neto <alessandroptsn@yahoo.com.br>
2022-02-23 20:28:22 -0300

    DT

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("reshape2","tidyverse","rvest","ggfortify","ggdendro","plotly","dplyr","shiny","shinyWidgets","readxl","tidyr","shinythemes","stringr","kableExtra","highcharter","ggplot2")
+my_packages = c("DT","reshape2","tidyverse","rvest","ggfortify","ggdendro","plotly","dplyr","shiny","shinyWidgets","readxl","tidyr","shinythemes","stringr","kableExtra","highcharter","ggplot2")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
